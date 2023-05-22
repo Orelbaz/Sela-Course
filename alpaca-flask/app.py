fdad583efc09060dfa8e34b7cc10ade6cd31894c
Orelbaz <orelbaz18@gmail.com>
2023-05-22 12:23:17 +0300

    test2

--- alpaca-flask/app.py
+++ alpaca-flask/app.py
@@ -20,8 +20,5 @@ def index():
     url = random.choice(images)
     return render_template("index.html", url=url)
 
-
-#fafafafa
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
