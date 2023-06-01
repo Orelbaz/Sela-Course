1639e995680cef1e0ed7502315ffca61b1e40c74
Orelbaz <orelbaz18@gmail.com>
2023-06-01 12:01:14 +0300

    alpca fix

--- alpaca-flask/app.py
+++ alpaca-flask/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 # list of alpaca images
 images = [
-    "http://www.gifbay.com/gif/welcome_to_the_alpacalypse-131876/",
+    
     "https://media3.giphy.com/media/QoncgdHL9TDm0ePySn/giphy.gif?cid=ecf05e47eqnn1ipow6vxu4crrsdhd46t2za1sh2bmgyvp0y6&ep=v1_gifs_search&rid=giphy.gif&ct=g",
     "https://media2.giphy.com/media/c8pTpUed78PSg/giphy.gif?cid=ecf05e473chqondqlden75jnqclpmstfpdwqkgxlshn134oo&ep=v1_gifs_related&rid=giphy.gif&ct=g",
     "https://media4.giphy.com/media/xT1XGDKwOxacd92rYI/giphy.gif?cid=ecf05e47e7yh3uq69oehvlgd2ubcycp6y0pa8c319uoxk5e0&ep=v1_gifs_related&rid=giphy.gif&ct=g",
-    "http://smoteymote.tumblr.com/post/85942167395/pantlesswerewolf-and-i-went-to-the-akron-zoo",
+    
 ]
 
 
