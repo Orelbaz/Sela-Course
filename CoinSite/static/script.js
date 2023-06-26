9eebecbc092b9d478ab87624e8bdda0dc72414f4
Orelbaz <orelbaz18@gmail.com>
2023-06-26 13:43:29 +0300

    new worth

--- CoinSite/static/script.js
+++ CoinSite/static/script.js
@@ -1,6 +1,32 @@
-function showNetWorth(button) {
+function showNetWorth(button, symbol) {
     var netWorth = button.nextElementSibling;
-    netWorth.classList.toggle('hidden');
+    var netWorthElement = document.getElementById(symbol + '-worth');
 
-    // Add additional effects here if desired
+    // Fetch the real-time net worth
+    fetchRealTimeNetWorth(symbol)
+        .then(function (realTimeNetWorth) {
+            netWorthElement.textContent = realTimeNetWorth + " $";
+            netWorth.classList.toggle('hidden');
+        })
+        .catch(function (error) {
+            console.log("Error fetching net worth:", error);
+        });
+}
+
+function fetchRealTimeNetWorth(symbol) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/get_realtime_price?symbol=' + symbol, true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject(new Error("Request failed. Status: " + xhr.status));
+            }
+        };
+        xhr.onerror = function () {
+            reject(new Error("Request failed. Network error."));
+        };
+        xhr.send();
+    });
 }
