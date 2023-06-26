6982bfeab769b16359fd22d66e38f0b68a7cf125
Orelbaz <orelbaz18@gmail.com>
2023-06-26 12:14:52 +0300

    python html

--- CoinSite/app.py
+++ CoinSite/app.py
@@ -1,15 +1,29 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+API_KEY = "NX6VLQOK2DEROL37"
+
+coins = [
+    {'name': 'Bitcoin', 'symbol': 'BTC', 'worth': ''},
+    {'name': 'Tesla', 'symbol': 'TSLA', 'worth': ''}
+]
+
+
+def get_realtime_price(symbol):
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data['Global Quote']['05. price']
+
+
 @app.route('/')
 def index():
-    coins = [
-        {'name': 'Bitcoin', 'worth': '$30,000'},
-        {'name': 'Tesla', 'worth': '$700'},
-        # Add more coins here
-    ]
+    for coin in coins:
+        coin['worth'] = get_realtime_price(coin['symbol'])
     return render_template('index.html', coins=coins)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
