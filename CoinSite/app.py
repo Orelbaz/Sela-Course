a9155a975207be54b0bf3830f5dde4756ce4bb85
Orelbaz <orelbaz18@gmail.com>
2023-06-26 15:48:27 +0300

    bug

--- CoinSite/app.py
+++ CoinSite/app.py
@@ -15,14 +15,31 @@ coins = [
     {'name': 'Google', 'symbol': 'GOOGL', 'worth': ''},
     {'name': 'Facebook', 'symbol': 'FB', 'worth': ''}
 ]
+User
+this is my app.py file pls change the necessry stuff and print it:
 
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+API_KEY = "YOUR_ALPHA_VANTAGE_API_KEY"
+
+coins = [
+    {'name': 'Bitcoin', 'symbol': 'BTC', 'worth': ''},
+    {'name': 'Tesla', 'symbol': 'TSLA', 'worth': ''},
+    {'name': 'Ethereum', 'symbol': 'ETH', 'worth': ''},
+    {'name': 'Amazon', 'symbol': 'AMZN', 'worth': ''},
+    {'name': 'Apple', 'symbol': 'AAPL', 'worth': ''},
+    {'name': 'Microsoft', 'symbol': 'MSFT', 'worth': ''},
+    {'name': 'Google', 'symbol': 'GOOGL', 'worth': ''},
+    {'name': 'Facebook', 'symbol': 'FB', 'worth': ''}
+]
 
 def get_realtime_price(symbol):
     url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
     response = requests.get(url)
     data = response.json()
-
-    print(data)  # Print the response data for debugging
 
     if 'Global Quote' in data and '05. price' in data['Global Quote']:
         return data['Global Quote']['05. price']
