f89b46a8ec32aff875592616a396ac9e39de003f
Orelbaz <orelbaz18@gmail.com>
2023-06-26 13:49:30 +0300

    bug

--- CoinSite/app.py
+++ CoinSite/app.py
@@ -1,9 +1,9 @@
 import requests
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_KEY = "YOUR_ALPHA_VANTAGE_API_KEY"
+API_KEY = "NX6VLQOK2DEROL37"
 
 coins = [
     {'name': 'Bitcoin', 'symbol': 'BTC', 'worth': ''},
@@ -15,23 +15,14 @@ def get_realtime_price(symbol):
     url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
     response = requests.get(url)
     data = response.json()
-
-    if 'Global Quote' in data and '05. price' in data['Global Quote']:
-        return data['Global Quote']['05. price']
-    else:
-        return 'N/A'
+    return data['Global Quote']['05. price']
 
 
 @app.route('/')
 def index():
+    for coin in coins:
+        coin['worth'] = get_realtime_price(coin['symbol'])
     return render_template('index.html', coins=coins)
-
-
-@app.route('/get_realtime_price')
-def get_realtime_price_route():
-    symbol = request.args.get('symbol')
-    price = get_realtime_price(symbol)
-    return jsonify(price)
 
 
 if __name__ == '__main__':
--- CoinSite/static/script.js
+++ CoinSite/static/script.js
@@ -1,32 +1,6 @@
-function showNetWorth(button, symbol) {
+function showNetWorth(button) {
     var netWorth = button.nextElementSibling;
-    var netWorthElement = document.getElementById(symbol + '-worth');
+    netWorth.classList.toggle('hidden');
 
-    // Fetch the real-time net worth
-    fetchRealTimeNetWorth(symbol)
-        .then(function (realTimeNetWorth) {
-            netWorthElement.textContent = realTimeNetWorth + " $";
-            netWorth.classList.toggle('hidden');
-        })
-        .catch(function (error) {
-            console.log("Error fetching net worth:", error);
-        });
-}
-
-function fetchRealTimeNetWorth(symbol) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/get_realtime_price?symbol=' + symbol, true);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                resolve(xhr.responseText);
-            } else {
-                reject(new Error("Request failed. Status: " + xhr.status));
-            }
-        };
-        xhr.onerror = function () {
-            reject(new Error("Request failed. Network error."));
-        };
-        xhr.send();
-    });
+    // Add additional effects here if desired
 }
