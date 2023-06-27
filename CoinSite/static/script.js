d35f569ab6222a238dbf839529da6f913c5fdda6
Or <134198819+Orelbaz@users.noreply.github.com>
2023-06-27 22:39:34 +0300

    Update script.js

--- CoinSite/static/script.js
+++ CoinSite/static/script.js
@@ -1,6 +1,1 @@
-function showStockWorth(button) {
-    var StockWorth = button.parentNode.querySelector('.stock-worth');
-    netWorth.classList.toggle('hidden');
 
-    // Add additional effects here if desired
-}
