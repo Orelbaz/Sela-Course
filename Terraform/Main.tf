24c65e70b508c72d530a937fa9d8b47aec54a255
Orelbaz <orelbaz18@gmail.com>
2023-08-03 14:19:37 +0300

    Update Main

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -1,14 +1,15 @@
-####provider "google" {
-##  credentials = file("~/home/or/Desktop/gke-Key.json")
-##  project     = "gke-first-393008"
-##  region      = "us-central1" 
-#}
-#
-# Include the GKE cluster configurations from the separate files
-#module "eks_prod_cluster" {
-#  source = "./GKE-Prod.tf"
-#}
-#
-#module "eks_test_cluster" {
-#  source = "./GKE-Test.tf"
-#}
+provider "google" {
+  credentials = file("~/home/or/Desktop/gke-Key.json")
+  project     = "gke-first-393008"
+  region      = "us-central1" 
+}
+
+# Include the GKE-Prod and GKE-Test cluster configurations
+module "eks_prod_cluster" {
+  source = "./GKE-Prod.tf"
+}
+
+module "eks_test_cluster" {
+  source = "./GKE-Test.tf"
+}
+
