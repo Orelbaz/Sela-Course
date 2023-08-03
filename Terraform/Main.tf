b29cf0e82b70281fb35d06e6c83f8e6dd5c4a1e4
Orelbaz <orelbaz18@gmail.com>
2023-08-03 14:13:42 +0300

    Remove Main

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -1,14 +1,14 @@
-provider "google" {
-  credentials = file("~/home/or/Desktop/gke-Key.json")
-  project     = "gke-first-393008"
-  region      = "us-central1" 
-}
-
+####provider "google" {
+##  credentials = file("~/home/or/Desktop/gke-Key.json")
+##  project     = "gke-first-393008"
+##  region      = "us-central1" 
+#}
+#
 # Include the GKE cluster configurations from the separate files
-module "eks_prod_cluster" {
-  source = "./GKE-Prod.tf"
-}
-
-module "eks_test_cluster" {
-  source = "./GKE-Test.tf"
-}
+#module "eks_prod_cluster" {
+#  source = "./GKE-Prod.tf"
+#}
+#
+#module "eks_test_cluster" {
+#  source = "./GKE-Test.tf"
+#}
